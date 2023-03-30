4471f53816c9e680d8ff11e716d69502eed16ee0
ChungHaLee <ssk04040@naver.com>
2023-03-30 21:09:17 +0900

    second commit

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -6,12 +6,7 @@ module.exports = {
     entry: {
         audio: path.resolve(__dirname, "src", "js", "audio.js"),
         circle: path.resolve(__dirname, "src", "js", "circle.js"),
-        sparkle: path.resolve(__dirname, "src", "js", "sparkle.js"),
-        starfield: path.resolve(__dirname, "src", "js", "starfield.js"),
-        vanta: path.resolve(__dirname, "src", "js", "vanta.js"),
-        birds: path.resolve(__dirname, "node_modules", "vanta", "src", "vanta.birds.js"),
-        colorpicker: path.resolve(__dirname, "src", "js", "colorpicker.js"),
-        fluid: path.resolve(__dirname, "src", "js", "fluid.js")
+        colorpicker: path.resolve(__dirname, "src", "js", "colorpicker.js")
     },
     mode: "development",
     target: "web",
