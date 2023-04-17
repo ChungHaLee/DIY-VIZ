851f5d2cbe91062725cb15b9e26ab41d53cf7c12
ChungHaLee <ssk04040@naver.com>
2023-04-17 16:13:23 +0900

    added analysis-real final

--- meq.py
+++ meq.py
@@ -4,6 +4,7 @@ from collections import OrderedDict
 from matplotlib.ticker import MaxNLocator
 import seaborn as sns
 import matplotlib.pyplot as plt
+from statannot import add_stat_annotation
 
 
 diyvisualdf = pd.read_excel('diyvisual.xlsx',  engine='openpyxl')
@@ -12,50 +13,53 @@ onlymusicdf = pd.read_excel('onlymusic.xlsx',  engine='openpyxl')
 
 nasatlxdf = pd.read_excel('nasatlx.xlsx',  engine='openpyxl')
 
+def printAnnotation(df, yvalue):
+    plt.figure(figsize=(9, 6))
+    plt.title(f'Music Entertainment Survey: {yvalue}')
+    plt.ylim([0, 8])
 
-
-plt.figure(figsize=(15, 10))
-plt.title('NASA TLX Survey')
-plt.ylim([0, 11]) 
-sns.boxplot(data=nasatlxdf)
-plt.savefig('./result/graphs/NASA-TLX/nasatlx.png', bbox_inches = 'tight')
-
-
-
-
-
-# Expression of Semantic Descriptors
-
-def mean_dataframe_byShape(inputdf, identifier):
-    try:
-        low_high = inputdf.mean()
-
-
-        dict = { 'low': low_high, 'weak': weak_loud, 'tonal': tonal_noisy, 'short': short_long, 'static': static_dynamic, 'smooth' : smooth_rough, 'dull': dull_bright, 'mate': mate_resonant, 'light': light_heavy , 'cold': cold_warm}
-        opposites = ('high', 'loud', 'noisy', 'long', 'dynamic', 'rough', 'bright', 'resonant', 'heavy', 'warm')
-        fig, ax1 = plt.subplots(1, 1)
-
-
-        ax1.plot(list(dict.values()), list(dict.keys()), marker='o', color='mediumvioletred')
-
-        ax1.set_yticks(np.r_[:len(dict)])
-        ax1.set_yticklabels(dict.keys())
-        ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
-
-        ax2 = ax1.twinx()
-        ax2.set_ylim(ax1.get_ylim())
-        ax2.set_yticks(np.r_[:len(dict)])
-        ax2.set_yticklabels(opposites)
+    ax = sns.boxplot(x=df['condition'], y=df[yvalue])
+    ax, test_results = add_stat_annotation(ax, data=df, x='condition', y=yvalue, box_pairs=[('onlyMusic', 'DIY')], test='Wilcoxon', text_format='star', loc='inside')
+    plt.savefig(f'./result/graphs/MEQ/{yvalue}_meq.png', bbox_inches = 'tight')
 
 
 
-        fig.tight_layout()
-        plt.title(f'Expression of Semantic Descriptors: {identifier}')
-        plt.xlim([0, 8]) 
-        plt.savefig(f'./result/graphs/shapes/{identifier}.png', bbox_inches = 'tight')
-        # plt.show()
-    except:
-        pass
+
+
+printAnnotation()
+# print(round(nasatlxdf['mental demand'].mean(), 2), round (nasatlxdf['mental demand'].std(), 2))
+# print(round(nasatlxdf['physical demand'].mean(), 2), round (nasatlxdf['physical demand'].std(), 2))
+# print(round(nasatlxdf['temporal demand'].mean(), 2), round (nasatlxdf['temporal demand'].std(), 2))
+# print(round(nasatlxdf['effort'].mean(), 2), round (nasatlxdf['effort'].std(), 2))
+# print(round(nasatlxdf['performance'].mean(), 2), round (nasatlxdf['performance'].std(), 2))
+# print(round(nasatlxdf['frustration'].mean(), 2), round (nasatlxdf['frustration'].std(), 2))
+
+
+print(round(diyvisualdf['stimulated'].mean(), 2), round (diyvisualdf['stimulated'].std(), 2))
+print(round(diyvisualdf['dancing'].mean(), 2), round (diyvisualdf['dancing'].std(), 2))
+print(round(diyvisualdf['entertain'].mean(), 2), round (diyvisualdf['entertain'].std(), 2))
+print(round(diyvisualdf['energized'].mean(), 2), round (diyvisualdf['energized'].std(), 2))
+print(round(diyvisualdf['moving'].mean(), 2), round (diyvisualdf['moving'].std(), 2))
+print(round(diyvisualdf['animated'].mean(), 2), round (diyvisualdf['animated'].std(), 2))
+print(round(diyvisualdf['excited'].mean(), 2), round (diyvisualdf['excited'].std(), 2))
+print(round(diyvisualdf['rhythm'].mean(), 2), round (diyvisualdf['rhythm'].std(), 2))
+
+# print(round(onlymusicdf['stimulated'].mean(), 2), round (onlymusicdf['stimulated'].std(), 2))
+# print(round(onlymusicdf['dancing'].mean(), 2), round (onlymusicdf['dancing'].std(), 2))
+# print(round(onlymusicdf['entertain'].mean(), 2), round (onlymusicdf['entertain'].std(), 2))
+# print(round(onlymusicdf['energized'].mean(), 2), round (onlymusicdf['energized'].std(), 2))
+# print(round(onlymusicdf['moving'].mean(), 2), round (onlymusicdf['moving'].std(), 2))
+# print(round(onlymusicdf['animated'].mean(), 2), round (onlymusicdf['animated'].std(), 2))
+# print(round(onlymusicdf['excited'].mean(), 2), round (onlymusicdf['excited'].std(), 2))
+# print(round(onlymusicdf['rhythm'].mean(), 2), round (onlymusicdf['rhythm'].std(), 2))
+
+
+
+# plt.figure(figsize=(10, 8))
+# plt.title('MEQ Survey: DIY Visualization Condition')
+# plt.ylim([0, 8]) 
+# sns.boxplot(data=diyvisualdf)
+# plt.savefig('./result/graphs/MEQ/DIYVISUAL.png', bbox_inches = 'tight')
 
 
 
