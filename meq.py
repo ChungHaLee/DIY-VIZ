7ae91f4a4415c6be21f21c576762a85de0eec8f8
ChungHaLee <ssk04040@naver.com>
2023-04-19 03:43:07 +0900

    added MEQ comparison

--- meq.py
+++ meq.py
@@ -4,29 +4,19 @@ from collections import OrderedDict
 from matplotlib.ticker import MaxNLocator
 import seaborn as sns
 import matplotlib.pyplot as plt
-from statannot import add_stat_annotation
 
 
 diyvisualdf = pd.read_excel('diyvisual.xlsx',  engine='openpyxl')
 onlymusicdf = pd.read_excel('onlymusic.xlsx',  engine='openpyxl')
+uniteddf = pd.read_excel('united.xlsx',  engine='openpyxl')
+dd = pd.melt(uniteddf,id_vars=['condition'],value_vars=['stimulated', 'dancing', 'entertain', 'energized',	'moving', 'animated', 'excited', 'rhythm'],var_name='MEQ features')
+plt.figure(figsize=(15, 6))
+plt.title('Comparison of MEQ Survey')
+plt.ylim([0, 8]) 
+sns.boxplot(x='MEQ features',y='value',data=dd,hue='condition')
+plt.show()
 
-
-nasatlxdf = pd.read_excel('nasatlx.xlsx',  engine='openpyxl')
-
-def printAnnotation(df, yvalue):
-    plt.figure(figsize=(9, 6))
-    plt.title(f'Music Entertainment Survey: {yvalue}')
-    plt.ylim([0, 8])
-
-    ax = sns.boxplot(x=df['condition'], y=df[yvalue])
-    ax, test_results = add_stat_annotation(ax, data=df, x='condition', y=yvalue, box_pairs=[('onlyMusic', 'DIY')], test='Wilcoxon', text_format='star', loc='inside')
-    plt.savefig(f'./result/graphs/MEQ/{yvalue}_meq.png', bbox_inches = 'tight')
-
-
-
-
-
-printAnnotation()
+# printAnnotation()
 # print(round(nasatlxdf['mental demand'].mean(), 2), round (nasatlxdf['mental demand'].std(), 2))
 # print(round(nasatlxdf['physical demand'].mean(), 2), round (nasatlxdf['physical demand'].std(), 2))
 # print(round(nasatlxdf['temporal demand'].mean(), 2), round (nasatlxdf['temporal demand'].std(), 2))
@@ -35,14 +25,14 @@ printAnnotation()
 # print(round(nasatlxdf['frustration'].mean(), 2), round (nasatlxdf['frustration'].std(), 2))
 
 
-print(round(diyvisualdf['stimulated'].mean(), 2), round (diyvisualdf['stimulated'].std(), 2))
-print(round(diyvisualdf['dancing'].mean(), 2), round (diyvisualdf['dancing'].std(), 2))
-print(round(diyvisualdf['entertain'].mean(), 2), round (diyvisualdf['entertain'].std(), 2))
-print(round(diyvisualdf['energized'].mean(), 2), round (diyvisualdf['energized'].std(), 2))
-print(round(diyvisualdf['moving'].mean(), 2), round (diyvisualdf['moving'].std(), 2))
-print(round(diyvisualdf['animated'].mean(), 2), round (diyvisualdf['animated'].std(), 2))
-print(round(diyvisualdf['excited'].mean(), 2), round (diyvisualdf['excited'].std(), 2))
-print(round(diyvisualdf['rhythm'].mean(), 2), round (diyvisualdf['rhythm'].std(), 2))
+# print(round(diyvisualdf['stimulated'].mean(), 2), round (diyvisualdf['stimulated'].std(), 2))
+# print(round(diyvisualdf['dancing'].mean(), 2), round (diyvisualdf['dancing'].std(), 2))
+# print(round(diyvisualdf['entertain'].mean(), 2), round (diyvisualdf['entertain'].std(), 2))
+# print(round(diyvisualdf['energized'].mean(), 2), round (diyvisualdf['energized'].std(), 2))
+# print(round(diyvisualdf['moving'].mean(), 2), round (diyvisualdf['moving'].std(), 2))
+# print(round(diyvisualdf['animated'].mean(), 2), round (diyvisualdf['animated'].std(), 2))
+# print(round(diyvisualdf['excited'].mean(), 2), round (diyvisualdf['excited'].std(), 2))
+# print(round(diyvisualdf['rhythm'].mean(), 2), round (diyvisualdf['rhythm'].std(), 2))
 
 # print(round(onlymusicdf['stimulated'].mean(), 2), round (onlymusicdf['stimulated'].std(), 2))
 # print(round(onlymusicdf['dancing'].mean(), 2), round (onlymusicdf['dancing'].std(), 2))
@@ -56,10 +46,10 @@ print(round(diyvisualdf['rhythm'].mean(), 2), round (diyvisualdf['rhythm'].std()
 
 
 # plt.figure(figsize=(10, 8))
-# plt.title('MEQ Survey: DIY Visualization Condition')
+# plt.title('MEQ Survey: Only Music Condition')
 # plt.ylim([0, 8]) 
-# sns.boxplot(data=diyvisualdf)
-# plt.savefig('./result/graphs/MEQ/DIYVISUAL.png', bbox_inches = 'tight')
+# sns.boxplot(data=onlymusicdf)
+# plt.savefig('./result/graphs/MEQ/ONLYMUSIC.png', bbox_inches = 'tight')
 
 
 
